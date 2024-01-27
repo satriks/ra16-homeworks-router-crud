a88b10efd6e4a47a5bfe54f5ebfbdf95fe05d86c
Yury Vereskun <satriks@mail.ru>
2024-01-27 04:31:15 +0300

    update env

--- src/components/utils/PostService.tsx
+++ src/components/utils/PostService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const connect = axios.create({
-  baseURL: "http://localhost:7070",
+  baseURL: import.meta.env.VITE_URL || "http://localhost:7070",
 });
 
 export default class PostService {
